7e58591d91270dc5cca9bd03b2e9c7653f72f3dd
willianpassarelli <devpassarelli@gmail.com>
2019-09-22 18:31:12 -0300

    inicializando aplicação

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["airbnb", "prettier", "prettier/react"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": ["warn", { extensions: [".jsx", "js"] }],
+    "import/prefer-default-export": "off",
+    "jsx-a11y/control-has-associated-label": "off",
+    "react/state-in-constructor": "off",
+    "no-console": ["error", { allow: ["tron"] }],
+    "no-param-reassign": "off"
+  }
+};
